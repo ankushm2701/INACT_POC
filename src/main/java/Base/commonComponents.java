63b2df31e07eb1f5681c712a89cbf424d1cd85fe
bprashanthkumar <bprashanthkumar@USHYDBPRASH09.us.deloitte.com>
2020-06-25 11:48:46 +0530

    added general methods

--- src/main/java/Base/commonComponents.java
+++ src/main/java/Base/commonComponents.java
@@ -1,16 +1,25 @@
 package Base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+
+import static org.testng.Assert.assertTrue;
 
 import java.util.concurrent.TimeUnit;
 
 public class commonComponents {
 
         public static WebDriver driver;
+        public static WebElement element;
+    	public static WebDriverWait wait;
+    	public static boolean bStatus;
 
         public void launchBrowser(String url){
             try {
@@ -31,5 +40,64 @@ public class commonComponents {
             driver.manage().window().maximize();
             driver.navigate().to(url);
         }
+        
+        
+        public static boolean clickButton(By locator)
+    	{
+    		try
+    		{
+    		driver.findElement(locator).click();
+    		return true;
+    		}
+    		catch(Exception ex)
+    		{
+    			return false;
+    			
+    		}
+    	}
+        
+        public static void insertText(By locator, String value)
+    	{
+    		try {
+    			driver.findElement(locator).sendKeys(value);
+    		}
+    		catch (Exception e) 
+    		{
+    			System.out.println(e);
+    		}
+    	}
+        
+        public static boolean waitForElementVisible(By locator,long timeOut) //Wait method for Element visibility 
+    	{
+    		try
+    		{
+    			wait = new WebDriverWait(driver, timeOut);
+    			element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+    			if(element!=null)
+    				return true;
+    			else
+    				return false;
+    		}
+    		catch(Exception e)
+    		{
+    			return false;
+    		}
+    	}
+        
+        
+        public static boolean AssertContains(By locator, String ExpectedValue) 
+  	  {
+  		  try
+  		  {
+  			  String elementText = driver.findElement(locator).getText();
+  			  assertTrue(elementText.contains(ExpectedValue));
+  			  return true;
+  		  }
+  		  catch (Exception e) 
+  		  {
+  			  System.out.println(e);
+  			  return false;
+  		  }
+  	  }
 }
 
