39e69d0901f39c1fae2632e1b283cc81e9a99b97
Podugu, Mani Rama Krishna <>
2020-07-13 15:42:02 +0530

    Excel path changing

--- src/main/java/Base/Constants.java
+++ src/main/java/Base/Constants.java
@@ -7,8 +7,9 @@ public class Constants {
     public static final String CHROME_DRIVER_EXE="src\\main\\resources\\chromedriver.exe";
     public  static  final String Property_File="src\\main\\resources\\config.properties";
     public  static  final String URL ="https://spartacus.c39j2-walkersde1-d4-public.model-t.cc.commerce.ondemand.com/electronics-spa/en/USD";
- // public  static  final String EXCEL_PATH = "src\\main\\resources\\Testdata.xlsx";
-    public  static  final String EXCEL_PATH  = "/Users/mpodugu/INACT_POC/src/main/resources/Testdata.xlsx";
+    public  static  final String EXCEL_PATH =System.getProperty("user.dir")+ "/src/main/resources/Testdata.xlsx";
+
+  
     public  static  final String EXCEL_SHEET_NAME = "Sheet1";
 
     public static final String Action= "action";
